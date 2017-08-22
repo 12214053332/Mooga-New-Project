de023c6c1da6e06e1c2ae2b51c5d9a7528f47da3
Eng.Sayed <Eng.Sayed>
2017-08-22 14:20:02 +0200

    Update Contact Us Page V1

--- views/mooga/contactus.php
+++ views/mooga/contactus.php
@@ -94,32 +94,38 @@
       <span class="clt-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing.<br><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.<br><br> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing.</span>
       <div class="from-list-lt">
         <div class="col-xs-12 col-lg-8 col-sm-12">
-          <form class="form-float form-alt">
+          <form class="contactus-form form-float form-alt" id="contactus-form" action="#" method="post">
             <div class="row">
-              <div class="form-group col-xs-12 col-sm-6"> <span class="from-input-ic"><i class="fa fa-user"></i></span>
-                
-
+              <div class="form-group col-xs-12 col-sm-6">
+                <select class="form-control cust-input-width cust-input-width-three zero-top-margin hi25 valid" name="type" id="type">
+											   <option value="">--اختر الغرض--</option>
+											   <option value="أستفسار">أستفسار</option>
+											   <option value="مقترح">مقترح</option>
+									       <option value="شكوى">شكوى</option>
+                         <option value="طلب تعاون">طلب تعاون</option>
+								</select>
               </div>
-              <div class="form-group col-md-9 col-lg-9 col-sm-9 col-xs-9"> <span class="from-input-ic"><i class="fa fa-user"></i></span>
-                <input type="text" class="form-control" id="name" name="name" placeholder="" value="">
+              <div class="form-group col-xs-12 col-sm-6">
+                <input type="text" class="form-control" id="name" name="name" placeholder="الاسم*" value="">
+                <span class="from-input-ic"><i class="fa fa-user"></i></span>
               </div>
               <div class="form-group col-xs-12 col-sm-6"> <span class="from-input-ic"><i class="fa fa-envelope-o"></i></span>
-                <input class="form-control" required="" placeholder="Email" type="text">
+                <input type="email" class="form-control" id="email" name="email" placeholder="الاميل" value="">
               </div>
               <div class="form-group col-xs-12 col-sm-6"> <span class="from-input-ic"><i class="fa fa-phone"></i></span>
-                <input class="form-control" required="" placeholder="Contact Number" type="text">
+                <input type="phone" class="form-control" id="phone" name="phone" placeholder="التليفون" value="">
               </div>
               <div class="form-group col-xs-12"> <span class="from-input-ic"><i class="fa fa-comments"></i></span>
-                <textarea class="form-control" required="" placeholder="Your Message"></textarea>
+                <textarea type="text" class="form-control" id="message" name="message" placeholder="الرساله"></textarea>
               </div>
-              <div class="form-group col-xs-12 col-sm-6">
+              <!--div class="form-group col-xs-12 col-sm-6">
                 <input class="verifi_code form-control" required="" placeholder="Verification Code" type="text">
-              </div>
-              <div class="form-group col-xs-12 col-sm-6">
+              </div-->
+              <!--div class="form-group col-xs-12 col-sm-6">
                 <div class="b-form-vertical__input"> <img id="captcha" src="images/captcha.png" alt=""> <a href="javascript:void(0);"><span class="captchareload"><i class="fa fa-refresh"></i></span></a> </div>
-              </div>
+              </div-->
               <div class="form-group col-xs-12">
-                <button class="btn pull-right" type="submit">Submit</button>
+                <button class="btn pull-right" type="submit">ارسال</button>
               </div>
             </div>
           </form>
@@ -130,7 +136,7 @@
               <div class="media-body">
                 <div class="lt-co-icon"> <img src="images/ic-loc.png" alt="ic-loc"> </div>
                 <div class="lt-co-blok-text">
-                  <div class="lt-co-title">Location</div>
+                  <div class="lt-co-title">الموقع</div>
                   <hr class="lt-co-yellow-hr">
                   <p class="lt-bdr-one">Lorem ipsum dolor sit amet, consectetur isicing elit. Recusandae repudiandae dolores...</p>
                 </div>
@@ -142,10 +148,10 @@
               <div class="media-body">
                 <div class="lt-co-icon"> <img src="images/ic-call.png" alt="ic-call"> </div>
                 <div class="lt-co-blok-text">
-                  <div class="lt-co-title">Phone | Email</div>
+                  <div class="lt-co-title">التليفون | الاميل</div>
                   <hr class="lt-co-green-hr">
                   <p class="lt-bdr-two"><b>IND</b> :- + 001 245 0154<br>
-                    <b>E-Mail</b> :- <a href="mailto:directorylisting@gmail.com">directorylisting@gmail.com</a><br>
+                    <b>الاميل</b> :- <a href="mailto:directorylisting@gmail.com">directorylisting@gmail.com</a><br>
                   </p>
                 </div>
               </div>
@@ -156,9 +162,9 @@
               <div class="media-body">
                 <div class="lt-co-icon"> <img src="images/ic-time.png" alt="ic-time"> </div>
                 <div class="lt-co-blok-text">
-                  <div class="lt-co-title">Working Hours</div>
+                  <div class="lt-co-title">ساعات العمل</div>
                   <hr class="lt-bg-blue-hr">
-                  <p class="lt-bdr-three"><b>Mon</b> :- Sat :- 9am - 6pm</p>
+                  <p class="lt-bdr-three"><b>الاحد</b> :- الخميس :- 10ص - 6م</p>
                 </div>
               </div>
             </div>
