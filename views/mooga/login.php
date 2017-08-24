bbfc6754424164306f16e05b6135621c974a4967
Mostafa Elnemer <most22491@gmail.com>
2017-08-24 16:57:36 +0200

    last update

--- views/mooga/login.php
+++ views/mooga/login.php
@@ -1,246 +1,60 @@
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-lg-8 col-xs-12 col-sm-12 center-table">
-            <h2 class="full-lines sm-font"><span class="black-font">تسجيل الدخول</span></h2>
-            <p class="grey-font md-font text-center">إبدأ رحلتك مع منصة موجة  بتسجيل الدخول أو تفعيل إشتراكك الذي كونته</p>
-            <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12 triangle-tabs">
-                <ul class="nav nav-tabs nav-justified zero-right-padding " role="tablist">
-                    <li role="presentation" class="<?php if ($currentpage=="login"){echo "active";} ?>"><a href="#login" aria-controls="login" role="tab" data-toggle="tab">الدخول إلى حسابك</a></li>
-                    <li role="presentation" class="<?php if ($currentpage=="signup"){echo "active";} ?>"><a href="#signup" aria-controls="signup" role="tab" data-toggle="tab">إنشاء حساب جديد</a></li>
-                </ul>
-                <div class="tab-content">
-                    <div role="tabpanel" class="col-md-12 col-lg-12 col-xs-12 col-sm-12 tab-pane <?php if ($currentpage=="login"){echo "active";} ?> dark-grey" id="login">
-                        <div class="row">
-                         
-							
-							 <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                <a class="btn facebook-btn" href="fblogin/fbconfig.php">من خلال حسابك في</a>
-                            </div>
-                            <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                <a class="btn linkedin-btn" href="linkedinuser/process.php">من خلال حسابك في</a>
-                            </div>
-							
-							
-                            <div class="col-md-10 col-lg-10 col-xs-11 col-sm-11 center-table">
-                                <h2 class="full-lines sm-font"><span class="black-font">أو من خلال حسابك</span></h2>
-                            </div>
-                            <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-                                <div class="col-md-10 col-lg-10 col-xs-10 col-sm-10 center-table">
-                                    <form class="login-form col-md-8 col-lg-8 col-xs-12 col-sm-12 center-table" action="?page=_usersaction&action=login" method="post" id="login-form" name="login-form">
-									
-                                        <div class="form-group col-md-12 col-lg-12 col-xs-12 col-sm-12">
-										    
-                                            <input type="email" id="uemail" name="email" class="form-control" value="<?php echo $rememberme->username; ?>" placeholder="البريد الإلكترونى" required >
-											<label for="uemail"></label>
-                                        </div>
-                                        <div class="form-group col-md-12 col-lg-12 col-xs-12 col-sm-12">
-										
-                                            <input type="password" id="upassword" name="password"  value="<?php echo $rememberme->password; ?>" class="form-control" placeholder="كلمة المرور" required >
-											<label for="upassword"></label>
-                                        </div>
-                                        <div class="form-group col-md-12 col-lg-12 col-xs-12 col-sm-12">
-                                            <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                                <a href="?page=forgetpassword" class="md-font blue-link text-right">نسيت كلمة السر</a>
-                                            </div>
-                                            <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                                <div class="checkbox">
-                                                    <label class="black-font md-font">
-                                                        <input name="remember_me" type="checkbox" value="1" <?php echo $rememberme->rememberme; ?>> تذكرني
-                                                    </label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group col-md-4 col-lg-4 col-xs-4 col-sm-4">
-                                            <div class="">
-                                                <button class="btn orange-btn">تسجيل الدخول</button>
-                                            </div>
-											
-                                        </div>
-										<div class="form-group">
-											<div id="login-response">
-                                                
-                                            </div>
-											 </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div role="tabpanel" class="col-md-12 col-lg-12 col-xs-12 col-sm-12 tab-pane <?php if ($currentpage=="signup"){echo "active";} ?> dark-grey" id="signup">
-                        <div class="row">
-                            <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                <a class="btn facebook-btn" href="fblogin/fbconfig.php">من خلال حسابك في</a>
-                            </div>
-                            <div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                <a class="btn linkedin-btn" href="linkedinuser/process.php">من خلال حسابك في</a>
-                            </div>
-                            <div class="col-md-10 col-lg-10 col-xs-11 col-sm-11 center-table">
-                                <h2 class="full-lines sm-font"><span class="black-font">أو من خلال حسابك</span></h2>
-                            </div>
-                            <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-                                <div class="col-md-10 col-lg-10 col-xs-10 col-sm-10 center-table">
-                                    <form class="login-form col-md-12 col-lg-12 col-xs-12 col-sm-12" action="?page=_usersaction&action=signup" method="post"  id="signup-form" name="sinup-form">
-                                        <div class="form-group">
-                                            <div id="signup-response">
-                                                
-                                            </div>
-                                        </div>
-										<div class="form-group col-md-12 col-lg-12 col-xs-12 col-sm-12">
-                                            <label for="" class="control-label"><span class="asterisc">*</span> الاسم </label>
-                                            <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-											
-											  <input type="text" class="form-control" id="name" name="name" placeholder="أدخل الاسم " required>
-											  
-                                            	<label for="name"></label>                                              
-											</div>
-                                        </div>
-                                        <div class="form-group col-md-12 col-lg-12 col-xs-12 col-sm-12">
-                                            <label for="" class="control-label"><span class="asterisc">*</span>البريد الإلكتروني</label>
-                                            <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-											
-                                                <input type="email" class="form-control" id="email" name="email" placeholder="أدخل البريد الإلكتروني" required>
-                                            <label for="email"></label>
-											</div>
-                                        </div>
-                                         
-										 	
-                                        <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-                                            <div class="form-group col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                                <label  class="control-label text-center"><span class="asterisc">*</span>الدولة</label>
-                                                <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-                                                  
-												<select class="form-control chosen-select" id="country" name="country" required>
-												
-													<option value="">اختر</option>
-													  <?php
-													 
-																foreach ($countries as $country) {
-																	$id=$country['id'];
-																	$name=$country['name'];
-																	$code=$country['code'];
-																	echo " <option value='$id'  data-code='$code' >$name</option> ";
-																}
-													 ?>
-                                                 </select>
-												 <label for="country"></label>
-                                                </div>
-                                            </div>
-                                        
-                                            <div class="form-group  col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                                <label  class=" control-label text-center">
-												
-												<span class="asterisc">*</span>المحافظة</label>
-                                                <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-                                                  
-												<select class="form-control chosen-select" id="states" name="states" >
-												
-													<option value="">اختر</option>
-													 
-                                                 </select>
-												 <label for="states"></label>
-                                                </div>
-                                            </div>
-                                        
-                                            <div class="form-group  col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                                <label  class="control-label text-center">
-												
-												<span class="asterisc">*</span>المدينة</label>
-                                                <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-                                                  
-												<select class="form-control chosen-select" id="cities" name="cities" >
-												
-													<option value="">اختر</option>
-													 
-                                                 </select>
-												 <label for="cities"></label>
-                                                </div>
-                                            </div>
-											
-										  <div class="form-group  col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                          
-											 <label  class=" control-label text-center"><span class="asterisc">*</span>الهاتف</label>
-                                            <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-											 	<div name="code" id="code" class="phone-code" ></div>
-                                                <input type="text" class="form-control" id="phone" name="phone" placeholder="أدخل الهاتف"  required>
-												<label for="phone"></label>
-                                            </div>
-                                        </div>
-											
-                                        </div>
-										
-										
-										     
-                                       
-                                       
-										
-					                
-										
-										<div class="form-group col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                            <label for="" class="control-label"><span class="asterisc">*</span>كلمة المرور</label>
-                                            <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-											
-                                                <input type="password" class="form-control" id="password" name="password" placeholder="أدخل كلمة المرور"  required>
-                                            <label for="password"></label>
-											</div>
-                                        </div>
-										
-										<div class="form-group col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                            <label for="" class=" control-label"><span class="asterisc">*</span>تأكيد كلمة المرور</label>
-                                            <div class="col-md-12 col-lg-12 col-xs-12 col-sm-12">
-												
-											  <input type="password" class="form-control" id="confirmpassword" name="confirmpassword" placeholder="تأكيد كلمة المرور"  required>
-											  <label for="confirmpassword"></label>                                              
-                                            </div>
-                                        </div>
-										
-										
-                                        <!--<div class="col-md-6 col-lg-6 col-xs-12 col-sm-12">
-                                            <div class="form-group">
-                                                <label for="" class="col-sm-6 control-label"><span class="asterisc">*</span>نوع الحساب </label>
-                                                <div class="col-sm-6">
-												
-                                                  <select class="form-control" id="account_type" name="account_type">
-                                                       <option value="">اختر</option>
-                                                       <option value="فردى">فردى</option>
-													   <option value="شركة">شركة</option>
-                                                 </select>
-                                                <label for="account_type"></label>     
-												</div>
-                                            </div>
-                                        </div>-->
-									
-                                        
-                                        <div class="form-group col-md-12 col-lg-12 col-xs-12 col-sm-12">
-                                            <div class="col-md-10 col-lg-10 col-xs-11 col-sm-11 center-table">
-											
-                                                <div class="checkbox center-form-text">
-												
-                                                    <label >
-                                                        <input type="checkbox"  id="agree" name="agree" required><span class="asterisc">*</span>أوافق على كافة <a target="_blank" href="?page=terms">الشروط والأحكام</a> اللازمة للتسجيل في الموقع
-                                                    </label>
-                                                </div>
-												<label for="agree"></label>
-                                            </div>
-                                        </div>
-                                        <div class="form-group col-md-4 col-lg-4 col-xs-12 col-sm-12">
-                                            <div class="">
-                                                <button class="btn orange-btn">تسجيل الدخول</button>
-                                            </div>
-                                        </div>
-
-										
-										
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
+<div id="breadcrum-inner-block">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="breadcrum-inner-header">
+                    <h1>تسجيل الدخول</h1>
+                    <a href="/">الرئيسية</a> <i class="fa fa-circle"></i> <a href="?page=login"><span>تسجيل الدخول</span></a> </div>
             </div>
         </div>
     </div>
 </div>
 
 
+
+<div id="vfx-product-inner-item">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-lg-offset-4 listing-modal-1">
+            <div class="text-center">
+                <h4 class="title-login-form">
+                    تسجيل الدخول
+                </h4>
+            </div>
+
+            <div class="clearfix"></div>
+            <div class="listing-login-form">
+                <form action="?page=_usersaction&action=login" id="login-form" name="login-form">
+                    <div class="listing-form-field"> <i class="fa fa-user blue-1"></i>
+                        <input class="form-field bgwhite" type="email" id="uemail" value="<?php echo $rememberme->username; ?>" name="email" placeholder="البريد الإلكترونى" required />
+                        <label for="uemail"></label>
+                    </div>
+                    <div class="listing-form-field"> <i class="fa fa-lock blue-1"></i>
+                        <input class="form-field bgwhite" type="password" name="password" value="<?php echo $rememberme->password; ?>" id="upassword" placeholder="كلمة المرور" required  />
+                        <label for="upassword"></label>
+                    </div>
+                    <div class="listing-form-field clearfix margin-top-20 margin-bottom-20">
+                        <input type="checkbox" id="checkbox-1-1" class="regular-checkbox" />
+                        <label for="checkbox-1-1"></label>
+                        <label class="checkbox-lable">تذكرني</label>
+                        <a href="?page=forgetpassword">نسيت كلمة السر</a> </div>
+                    <div class="listing-form-field">
+                        <input class="form-field submit" type="submit" value="تسجيل الدخول" />
+                    </div>
+                    <div class="form-group">
+                        <div id="login-response">
+                        </div>
+                    </div>
+                </form>
+
+                <div class="bottom-links">
+                    <p>ليس لديك حساب<a href="?page=signup">مستخدم جديد</a></p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+</div>
